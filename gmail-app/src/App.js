74567ef50da569397c1d64dd38fc5b86897b6eed
wwhite11 <2winstonrwhite@gmail.com>
2021-04-05 22:06:07 -0400

    sidebar almost complete

---
+++ gmail-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import Header from "./Header";
+import Sidebar from "./Sidebar";
+
+function App() {
+  return (
+    <div className='App'>
+      <Header />
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App;
